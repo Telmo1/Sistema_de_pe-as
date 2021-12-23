fd19aedfe63b0a762a7769f6b7aeec982ce59912
Telmo <yrtelmo00@gmail.com>
2021-12-22 21:23:20 -0300

    Sistema de peças Final

---
+++ pecas.js
@@ -0,0 +1,36 @@
+// Váriaveis, são  um espaço na memoria do computador, para guardar algum valor!
+let pesoPeca = 80
+
+
+if (pesoPeca > 100) {
+    console.log("Peso maior que 100g")
+} else {
+    console.log("Peso insuficiente, não é possivel completar operação")
+}
+
+let numeroPecas = 11
+
+if (numeroPecas < 10) {
+    console.log("Ainda há espaço na caixa, Podemos cadastrar")
+} else {
+    console.log("Não é possivel continuar cadastrando, não á espaço na caixa")
+}
+
+let caracteresPeca = "Di"
+
+console.log("o comprimento do nome da peça é:", caracteresPeca.length)
+
+if (caracteresPeca.length < 3) {
+    console.log("Erro, caracteres insufucientes")
+
+} else {
+    console.log("Cadastro aprovado")
+}
+
+
+
+
+
+
+
+
